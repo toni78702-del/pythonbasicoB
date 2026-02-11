390ff3c49a72445b558cc8a2a34dee8cd7567f70
artaustin09 <a.r.t.austin09@gmail.com>
2026-02-10 19:09:42 -0800

    documento

---
+++ sesion_1.py
@@ -0,0 +1,16 @@
+# numerio y flotantes 
+
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.7))
+print(int(1+2))
+print(int(10*2))
+
+print("===== operadores matematicos ======")
+# +
+# -
+# *
+# /
+# **
+# % modulo
