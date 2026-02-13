6ddfbaea17658db7e3d03eaa91a5aef689cd824c
toni78702-del <toni78702@gmail.com>
2026-02-12 18:42:01 -0800

    documento el funcionamiento de las listas en python

---
+++ sesion_2.py
@@ -0,0 +1,9 @@
+#tuplas
+mi_tupla = (2,4)
+print("mi tuple",mi_tupla)
+
+#lista
+mi_tupla = [1,3,3.1416,"christian",mi_tupla]
+print("el primer elemento de mi lista :",mi_tupla[0])
+print("el cuarto elemento de mi lista :",mi_tupla[3])
+print("el tercer elemento de mi lista :",mi_tupla[2])
