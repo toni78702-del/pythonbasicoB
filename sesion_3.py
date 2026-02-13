83d491b14c9c47bd3953417b167ffb3227084848
toni78702-del <toni78702@gmail.com>
2026-02-13 15:31:27 -0800

    como añidio un wgile

--- sesion_3.py
+++ sesion_3.py
@@ -1,5 +1,5 @@
 # loops 
-mi_lista = [1,2,3,4,45]
+mi_lista = [1,2,3,4,5]
 
 for i in mi_lista:
     print("el numero es",i)
@@ -17,3 +17,17 @@ mi_lista_2 = ["lunes","martes","miercoles","jueves","viernes"]
 for i in mi_lista_2:
     if i != "lunes":
         print(f"feliz {i}!")
+
+#whilw loop
+i = 0
+
+while i < 5:
+    i += 1
+    if i==3:
+        continue
+    print(i)
+    if i==4:
+        break
+else:
+    print("i e ahora mayor o igual a 5")
+
